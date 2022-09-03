3b124f20aabf3799a9d7a6ef8138fb2b65387aea
ivusc <100025097+ivusc@users.noreply.github.com>
2022-09-03 22:37:15 +0800

    bug fixes

--- lib/theme.ts
+++ lib/theme.ts
@@ -3,7 +3,7 @@ import { mode,  } from "@chakra-ui/theme-tools";
 
 export const theme = extendTheme({
   config:{
-     initialColorMode: 'light',
+     initialColorMode: 'dark',
      useSystemColorMode: false,
   },
   fonts:{
